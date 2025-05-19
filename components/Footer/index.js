916c9c75ad20884d4bf43373795cf0a92b7c9e86
StanislavJS <stasyk55@gmail.com>
2025-05-19 14:21:48 +0300

    new blog

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,13 +8,13 @@ const Footer = ({}) => {
     <>
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
         <div>
-          <h1 className="text-2xl text-bold">Contact</h1>
+          <h1 className="text-2xl text-bold">Contact.</h1>
           <div className="mt-10">
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              LET&apos;S WORK
+            LET&apos;S
             </h1>
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              TOGETHER
+              CONNECT
             </h1>
             <Button type="primary">+370 630 82 641</Button>
             <div className="mt-10">
